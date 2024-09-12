dd61bc5dff2784cdbc1bf9f1990daf8675979f6e
Abdelhadi <twitchabdelhadi@gmail.com>
2024-09-12 15:11:55 +0100

    named links in navBar

--- src/components/generaleComponents/navBar/navbar.jsx
+++ src/components/generaleComponents/navBar/navbar.jsx
@@ -24,45 +24,43 @@ export default function Navbar() {
     return(
         <header className="header" >
             <nav className="nav container">
-                <a className="nav__logo" href="#home">Abdelhadi</a>
+                <a className="nav__logo" >عبادتي</a>
                 <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
 
                         <li className="nav__item" onClick={() => { setActiveLink("home") }}>
-                            <a href="#home" className={activeLink === "home" ? "nav__link active-link" : "nav__link" } >
+                            <a  className={activeLink === "home" ? "nav__link active-link" : "nav__link" } >
                                 <i className="uil uil-estate nav__icon"></i>
                                 
-                                 Home
+                                مواقيت الصلاة
                             </a>
                         </li>
 
                         <li className="nav__item" onClick={() => { setActiveLink("about") }}>
-                            <a href="#about"  className={activeLink === "about" ? "nav__link active-link" : "nav__link" }>
-                                <i className="uil uil-user nav__icon"></i> About
+                            <a   className={activeLink === "about" ? "nav__link active-link" : "nav__link" }>
+                                <i className="uil uil-user nav__icon"></i> 
+                                القرآن الكريم
                             </a>
                         </li>
 
                         <li className="nav__item" onClick={() => { setActiveLink("skills") }}>
-                            <a href="#skills" className={activeLink === "skills" ? "nav__link active-link" : "nav__link" } >
-                                <i className="uil uil-file-alt nav__icon"></i>Skills
+                            <a  className={activeLink === "skills" ? "nav__link active-link" : "nav__link" } >
+                                <i className="uil uil-file-alt nav__icon"></i>
+                                 الأحاديث النبوية
                             </a>
                         </li>
 
                         <li className="nav__item" onClick={() => { setActiveLink("Services") }}>
-                            <a href="#" className={activeLink === "Services" ? "nav__link active-link" : "nav__link" }>
-                                <i className="uil uil-briefcase nav__icon"></i>Services
+                            <a  className={activeLink === "Services" ? "nav__link active-link" : "nav__link" }>
+                                <i className="uil uil-briefcase nav__icon"></i>
+                                الدروس و الفتاوى
                             </a>
                         </li>
 
                         <li className="nav__item" onClick={() => { setActiveLink("Portfolio") }}>
-                            <a href="#portfolio" className={activeLink === "Portfolio" ? "nav__link active-link" : "nav__link" }>
-                                <i className="uil uil-scenery nav__icon"></i>Portfolio
-                            </a>
-                        </li>
-
-                        <li className="nav__item" onClick={() => { setActiveLink("Contact") }}>
-                            <a href="#contact" className={activeLink === "Contact" ? "nav__link active-link" : "nav__link" }>
-                                <i className="uil uil-message nav__icon"></i> Contact
+                            <a  className={activeLink === "Portfolio" ? "nav__link active-link" : "nav__link" }>
+                                <i className="uil uil-scenery nav__icon"></i>
+                                تواصل معنا
                             </a>
                         </li>
 
