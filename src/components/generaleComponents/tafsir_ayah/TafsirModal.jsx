d14081fd28e59498097008500dc93ebc7ee3eb60
Abdelhadi <twitchabdelhadi@gmail.com>
2024-09-15 11:57:49 +0100

    fixing responsive 2

--- src/components/generaleComponents/tafsir_ayah/TafsirModal.jsx
+++ src/components/generaleComponents/tafsir_ayah/TafsirModal.jsx
@@ -49,14 +49,14 @@ const TafsirModal = () => {
         <div onClick={() => setTafsirAya(false)} className='tafsir_Modal'>
             <div className="tafirModelConte" onClick={(e) => e.stopPropagation()} >
                 <div className="tafasir_buttons">
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-ibn-kathir') }  className={tafsirId == 'ar-tafsir-ibn-kathir' ? 'activeTafsirButton' : ''} > تفسير ابن كثير </button>
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-al-tabari') }  className={tafsirId == 'ar-tafsir-al-tabari' ? 'activeTafsirButton' : ''} > تفسير الطبري </button>
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-muyassar') }  className={tafsirId == 'ar-tafsir-muyassar' ? 'activeTafsirButton' : ''} > التفسير الميسر </button>
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafseer-al-saddi') }  className={tafsirId == 'ar-tafseer-al-saddi' ? 'activeTafsirButton' : ''} > تفسير السعدي </button>
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-al-wasit') }  className={tafsirId == 'ar-tafsir-al-wasit' ? 'activeTafsirButton' : ''} > تفسير الوسيط لطنطاوي </button>
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-al-baghawi') }  className={tafsirId == 'ar-tafsir-al-baghawi' ? 'activeTafsirButton' : ''} > تفسير البغوي </button>
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafseer-al-qurtubi') }  className={tafsirId == 'ar-tafseer-al-qurtubi' ? 'activeTafsirButton' : ''} > تفسير القرطبي </button>
-                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('en-tafisr-ibn-kathir') }  className={tafsirId == 'en-tafisr-ibn-kathir' ? 'activeTafsirButton' : ''} > Tafsir Ibn Kathir </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-ibn-kathir') }  className={tafsirId == 'ar-tafsir-ibn-kathir' ? 'activeTafsirButton' : ''} >  ابن كثير </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-al-tabari') }  className={tafsirId == 'ar-tafsir-al-tabari' ? 'activeTafsirButton' : ''} >  الطبري </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-muyassar') }  className={tafsirId == 'ar-tafsir-muyassar' ? 'activeTafsirButton' : ''} > ال الميسر </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafseer-al-saddi') }  className={tafsirId == 'ar-tafseer-al-saddi' ? 'activeTafsirButton' : ''} >  السعدي </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-al-wasit') }  className={tafsirId == 'ar-tafsir-al-wasit' ? 'activeTafsirButton' : ''} >  الوسيط لطنطاوي </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafsir-al-baghawi') }  className={tafsirId == 'ar-tafsir-al-baghawi' ? 'activeTafsirButton' : ''} >  البغوي </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('ar-tafseer-al-qurtubi') }  className={tafsirId == 'ar-tafseer-al-qurtubi' ? 'activeTafsirButton' : ''} >  القرطبي </button>
+                    <button   disabled={loading ? true : false}  onClick={ () => handleTafsirId('en-tafisr-ibn-kathir') }  className={tafsirId == 'en-tafisr-ibn-kathir' ? 'activeTafsirButton' : ''} >  Ibn Kathir </button>
                 </div>
 
                 <div className="tafsir_text">
